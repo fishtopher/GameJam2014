d2feaaa0f4965cf41507dfb2da5ac0d4f7758b78
Chris McLaughlin <fishtopher@gmail.com>
2014-01-25 21:30:43 +0900

    game flow

---
+++ Assets/Scripts/Game/Intro.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Intro : MonoBehaviour {
+
+	public void PlayAnim()
+	{
+		printf.PrintPersistentMessage("letsgo");
+		animation.Play("GetReadyAnim");
+	}
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(!animation.isPlaying)
+			gameObject.SetActive(false);
+	}
+	
+	public void ScreenIsCovered()
+	{
+		
+	}
+
+	public void ScreenIsUncovered()
+	{
+		TheGame.Instance.StartGame();	
+	}
+}
--- Assets/Scripts/Game/TheGame.cs
+++ Assets/Scripts/Game/TheGame.cs
@@ -4,22 +4,44 @@ using InControl;
 
 public class TheGame : MonoBehaviour {
 
+	public static TheGame Instance;
+
+	public Level m_level;
+
+	//Transitions
+	public GameObject m_titleScene;
+	public GameObject m_introScene;
+	public GameObject m_gameScene;
+	public GameObject m_endScene;
+
 	// Use this for initialization
-	void Start () 
+	void Awake () 
 	{
+		Instance = this;
 		InputManager.Setup();
-		printf.PrintPersistentMessage("Lets go");
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
 		InputManager.Update();
+	}
+	
+	public void PlayIntro()
+	{
+		printf.PrintPersistentMessage("playintro");
+		
+		m_introScene.SetActive(true);
+		m_gameScene.SetActive(false);
 
+	}
 
-		if(InputManager.ActiveDevice.GetControl(InputControlType.Start).WasPressed)
-		{
-			printf.PrintPersistentMessage("Start Pressed");
-		}
+	public void StartGame()
+	{
+		m_titleScene.SetActive(false);
+		m_endScene.SetActive(false);
+		m_gameScene.SetActive(true);
+		m_level.Reset();
+		
 	}
 }
