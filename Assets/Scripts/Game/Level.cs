8c5852b73c817713103fd931c0558c6138cfa58e
Christopher McLaughlin <chris@Christophers-MacBook-Pro.local>
2014-01-25 13:06:02 +0900

    level generation

--- Assets/Scripts/Game/Level.cs
+++ Assets/Scripts/Game/Level.cs
@@ -11,20 +11,36 @@ public class Level : MonoBehaviour
 	public VMath.FloatRange m_itemSpacing = new VMath.FloatRange(1, 2);
 
 	public GameObject[] m_itemPrefabs;
+	public GameObject m_bgStartPrefab;
+	public GameObject[] m_bgPrefabs;
+	public GameObject m_bgEndPrefab;
 
 
 	// Use this for initialization
 	void Start () 
 	{
-		float xpos = m_firstItem;
+		GameObject go;
+		float itemX = m_firstItem;
 		float yMin = -(m_laneWidth + m_laneSpacing) * ((float)m_numLanes-1) * 0.5f;
 		for(int i = 0; i < m_numItems; i++)
 		{
-			xpos += m_itemSpacing.RandomValue;
+			itemX += m_itemSpacing.RandomValue;
 			float ypos = yMin + Random.Range(0,m_numLanes) * (m_laneWidth + m_laneSpacing);
-			Vector3	p = new Vector3(xpos, ypos, 0);
-			GameObject go = Util.InstantiatePrefab( m_itemPrefabs[ Random.Range(0, m_itemPrefabs.Length) ], p); 
+			Vector3	p = new Vector3(itemX, ypos, 0);
+			Util.InstantiatePrefab( m_itemPrefabs[ Random.Range(0, m_itemPrefabs.Length) ], p); 
 		}
+
+		float bgX = 0;
+		go = Util.InstantiatePrefab( m_bgStartPrefab,  new Vector3(0, 0, 2)); 
+		bgX += go.transform.localScale.x;
+
+		while(bgX < itemX)
+		{
+			go = Util.InstantiatePrefab( m_bgPrefabs[ Random.Range(0, m_bgPrefabs.Length) ],  new Vector3(bgX, 0, 2)); 
+			bgX += go.transform.localScale.x;
+		}
+		Util.InstantiatePrefab( m_bgEndPrefab,  new Vector3(bgX, 0, 2)); 
+		
 	}
 	
 	// Update is called once per frame
